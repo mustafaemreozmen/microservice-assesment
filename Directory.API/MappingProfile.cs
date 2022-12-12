9016f61232747ff8bc9ee14848a9197020793471
Mustafa Emre Ozmen <mustafa.eozmen@gmail.com>
2022-12-13 00:17:15 +0300

    Add automapper profile.

---
+++ Directory.API/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Directory.API.DTOs;
+using Directory.Data.Entities;
+
+namespace Directory.API
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<PersonDTO, Person>();
+            CreateMap<InformationDTO, Information>();
+        }
+    }
+}
