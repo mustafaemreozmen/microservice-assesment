367b3f0c0c4f4ff4af819f86a4984740c2438f8a
Mustafa Emre Ozmen <mustafa.eozmen@gmail.com>
2022-12-12 23:57:34 +0300

    Add Person and PersonId to Information entity.

--- Directory.Data/Entities/Information.cs
+++ Directory.Data/Entities/Information.cs
@@ -7,4 +7,6 @@ public class Information : BaseEntity
     public InformationType InformationType { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
+    public Guid PersonId { get; set; }
+    public Person? Person { get; set; }
 }
