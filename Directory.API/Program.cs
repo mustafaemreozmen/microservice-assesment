3aeb03c9b3011edd4d28ecec798d67b446a589a5
Mustafa Emre Ozmen <mustafa.eozmen@gmail.com>
2022-12-13 00:14:41 +0300

    Add connection string.

--- Directory.API/Program.cs
+++ Directory.API/Program.cs
@@ -1,12 +1,21 @@
+using Directory.Data;
+using Directory.Data.Repositories;
+using Directory.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(Program));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<DirectoryContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DirectoryDb")));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
